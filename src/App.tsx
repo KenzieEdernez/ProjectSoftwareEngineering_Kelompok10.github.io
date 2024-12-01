98de6c9b424368cc10d7625c964417bbc39798db
Yoseph Yusanto <yosephyusanto@gmail.com>
2024-12-01 23:02:30 +0700

    adding login and register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { Navbar } from './components/navbar'
-import './App.css'
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Register } from "./pages/Register";
+import "./App.css";
+import { Login } from "./pages/Login";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowRegister(false);
+    setShowLogin(true);
+  };
+
+  const handleRegisterClick = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  }
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar onLoginClick={handleLoginClick} onRegisterClick={handleRegisterClick}/>
+      {showLogin && <Login />}
+      {showRegister && <Register />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
