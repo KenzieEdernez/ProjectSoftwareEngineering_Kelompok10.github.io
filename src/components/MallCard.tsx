ccc99e1870a3852c108a0edf04980efa9d18b358
KevinPangsawira <kevinpangsawira@gmail.com>
2024-12-04 12:19:59 +0700

    Change Theatre

--- src/components/MallCard.tsx
+++ src/components/MallCard.tsx
@@ -1,14 +1,12 @@
 interface MallCardProps{
-    images: string,
     mallName: string,
     location: string
   }
-function MallCard({images, mallName, location} : MallCardProps){
+function MallCard({ mallName, location} : MallCardProps){
     return(
    
             <div className="mall-card">
-                        <a href=""><img src={images} alt="" /></a>
-                        <h3>{mallName}</h3>
+                        <a href=""><h3>{mallName}</h3></a>
                         <p>{location}</p>
             </div>
       
--- src/pages/Theaters.tsx
+++ src/pages/Theaters.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import MallCard from "../components/MallCard";
-import CentralParkImage from "../assets/gambarMall/Central-Park-Mall-exterior-scaled.jpg";
-import TamanAnggrekImage from "../assets/gambarMall/taman-anggrek.jpg";
+// import CentralParkImage from "../assets/gambarMall/Central-Park-Mall-exterior-scaled.jpg";
+// import TamanAnggrekImage from "../assets/gambarMall/taman-anggrek.jpg";
 
 
 
 const malls = [
-  { images: CentralParkImage ,mallName: "Central Park", location: "Jalan X" },
-  { images: TamanAnggrekImage, mallName: "Taman Anggrek", location: "Jalan Y" },
-  { images: CentralParkImage ,mallName: "Central Park", location: "Jalan X" },
-  { images: TamanAnggrekImage, mallName: "Taman Anggrek", location: "Jalan Y" },
-  { images: CentralParkImage ,mallName: "Central Park", location: "Jalan X" },
-  { images: TamanAnggrekImage, mallName: "Taman Anggrek", location: "Jalan Y" },
+  { mallName: "Central Park", location: "Jalan X" },
+  {  mallName: "Taman Anggrek", location: "Jalan Y" },
+  { mallName: "Central Park", location: "Jalan X" },
+  { mallName: "Taman Anggrek", location: "Jalan Y" },
+  {mallName: "Central Park", location: "Jalan X" },
+  {  mallName: "Taman Anggrek", location: "Jalan Y" },
 ];
 
 
@@ -23,7 +23,7 @@ export const Theaters = () => {
     
     <div className="mall-card-container">
       {malls.map( (mall, i) =>(
-          <MallCard key={i} images={mall.images} mallName={mall.mallName} location={mall.location}/>
+          <MallCard key={i} mallName={mall.mallName} location={mall.location}/>
       ))
       }
     </div>
